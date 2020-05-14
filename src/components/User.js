4e23a6e84b9fdbf44b1e459893a20ccd4b712443
Clara Adora <claraadora@gmail.com>
2020-05-14 17:00:26 +0800

    Fixed minor issues

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export const User = () => {
-  const user = useSelector((state) => state.loggedUser);
-  console.log(user);
-  return <h5>Welcome, {user.email} </h5>;
+  const data = useSelector((state) => state.loggedUser);
+  return <h5>Welcome, {data.loggedUser.email} </h5>;
 };
